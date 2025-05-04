df43e1f835916b45b2f4998b48907435cba7d1e0
George Perdrizet <george@perdrizet.org>
2025-05-03 22:21:13 -0400

    Added pipeline generation method.

--- engineered_datasets/dataset.py
+++ engineered_datasets/dataset.py
@@ -1,6 +1,9 @@
 '''Generates variations of a dataset using a pool of feature engineering
 techniques. Used for training ensemble models.'''
 
+from pathlib import Path
+from random import choice, shuffle
+
 import h5py
 import pandas as pd
 
@@ -9,31 +12,80 @@ class DataSet:
 
     def __init__(
             self,
-            dataset_file: str,
             train_data: pd.DataFrame,
-            test_data: pd.DataFrame=None,
-            string_features: list=None
+            test_data: pd.DataFrame = None,
+            string_features: list = None
         ):
 
+        # Type check the user arguments and assign them to attributes
         if isinstance(train_data, pd.DataFrame):
-            self.dataset_file=dataset_file
-        else:
-            raise TypeError('Dataset file name is not a string.')
-
-        if isinstance(train_data, pd.DataFrame):
-            self.train_data=train_data
+            self.train_data = train_data
 
         else:
             raise TypeError('Train data is not a Pandas DataFrame.')
 
-        if isinstance(train_data, pd.DataFrame) or test_data is None:
-            self.test_data=test_data
+        if isinstance(test_data, pd.DataFrame) or test_data is None:
+            self.test_data = test_data
 
         else:
             raise TypeError('Test data is not a Pandas DataFrame.')
 
-        if isinstance(train_data, pd.DataFrame) or test_data is None:
-            self.string_features=string_features
+        if isinstance(string_features, list) or string_features is None:
+            self.string_features = string_features
 
         else:
             raise TypeError('String features is not a list.')
+
+        # Create the HDF5 output
+        Path('data').mkdir(parents=True, exist_ok=True)
+
+        with h5py.File('data/dataset.hdf5', 'a') as hdf:
+            _ = hdf.require_group('train')
+            _ = hdf.require_group('test')
+
+        # Define the feature engineering pipeline operations
+        self.string_encodings={
+            'onehot_encoding': {},
+            'ordinal_encoding': {}
+        }
+
+        self.engineerings={
+            'poly_features': {
+                'degree': [2, 3],
+                'interaction_only': [True, False],
+            },
+            'spline_features': {
+                'n_knots': [3, 4, 5],
+                'degree': [2, 3, 4],
+                'knots': ['uniform', 'quantile'],
+                'extrapolation': ['error', 'constant', 'linear', 'continue', 'periodic']
+            }
+        }
+
+
+    def _generate_data_pipeline(self):
+        '''Generates one random sequence of feature engineering operations. Starts with
+        a string encoding method if we have string features'''
+
+        pipeline={}
+
+        if self.string_features is not None:
+            options=list(self.string_encodings.keys())
+            selection=choice(options)
+            pipeline[selection]=self.string_encodings[selection]
+
+        operations=list(self.engineerings.keys())
+        shuffle(operations)
+
+        for operation in operations:
+
+            pipeline[operation]={}
+            parameters=self.engineerings[operation]
+
+            for parameter, values in parameters.items():
+
+                value=choice(values)
+                pipeline[operation][parameter]=value
+
+        return pipeline
+
--- tests/test.py
+++ tests/test.py
@@ -1,30 +1,113 @@
-'''Unittest for dataset class.'''
+'''Unittests for dataset class.'''
 
 import unittest
+import h5py
 import pandas as pd
 import engineered_datasets.dataset as ds
 
-class TestDataSet(unittest.TestCase):
-    '''Test class for main data set generator class'''
+class TestDataSetInit(unittest.TestCase):
+    '''Tests for main data set generator class initialization.'''
+
+    def setUp(self):
+        '''Dummy DataFrames and datasets for tests.'''
+
+        self.dummy_df_without_strings = pd.DataFrame({
+            'feature1': [0,1],
+            'feature2': [3,4],
+            'feature3': [5,6]
+        })
+
+        self.dataset_without_string_feature = ds.DataSet(
+            self.dummy_df_without_strings,
+            test_data=self.dummy_df_without_strings
+        )
+
+        self.dummy_df_with_strings = pd.DataFrame({
+            'feature1': [0,1],
+            'feature2': [3,4],
+            'feature3': ['a', 'b']
+        })
+
+        self.dataset_with_string_feature = ds.DataSet(
+            self.dummy_df_with_strings,
+            test_data=self.dummy_df_with_strings,
+            string_features=['feature3']
+        )
+
 
     def test_class_arguments(self):
         '''Tests assignments of class attributes from user arguments.'''
 
-        dummy_df=pd.DataFrame({'col1': [0,1], 'col2': [3,4]})
+        self.assertTrue(isinstance(self.dataset_with_string_feature.train_data, pd.DataFrame))
+        self.assertTrue(isinstance(self.dataset_with_string_feature.test_data, pd.DataFrame))
+        self.assertTrue(isinstance(self.dataset_with_string_feature.string_features, list))
+        self.assertEqual(self.dataset_with_string_feature.string_features[0], 'feature3')
 
-        dataset=ds.DataSet(
-            'test.h5',
-            dummy_df,
-            test_data=dummy_df,
-            string_features=['feature1', 'feature2']
+        with self.assertRaises(TypeError):
+            ds.DataSet(
+                'Not a Pandas Dataframe',
+                test_data=self.dummy_df_with_strings,
+                string_features=['feature3']
+            )
+
+        with self.assertRaises(TypeError):
+            ds.DataSet(
+                self.dummy_df_with_strings,
+                test_data='Not a Pandas Dataframe',
+                string_features=['feature3']
+            )
+
+        with self.assertRaises(TypeError):
+            ds.DataSet(
+                self.dummy_df_with_strings,
+                test_data=self.dummy_df_with_strings,
+                string_features='Not a list of features'
+            )
+
+
+    def test_output_creation(self):
+        '''Tests the creation of the HDF5 output sink.'''
+
+        hdf = h5py.File('data/dataset.hdf5', 'a')
+
+        self.assertTrue('train' in hdf)
+        self.assertTrue('test' in hdf)
+
+
+    def test_pipeline_options(self):
+        '''Tests the creation of feature engineering pipeline options'''
+
+        self.assertTrue(isinstance(self.dataset_with_string_feature.string_encodings, dict))
+        self.assertTrue(isinstance(self.dataset_with_string_feature.engineerings, dict))
+
+
+class TestDataPipelineGen(unittest.TestCase):
+    '''Tests for data pipeline generator function.'''
+
+    def setUp(self):
+        '''Dummy DataFrames and datasets for tests.'''
+
+        self.dummy_df = pd.DataFrame({
+            'feature1': [0,1],
+            'feature2': [3,4],
+            'feature3': ['a', 'b']
+        })
+
+        self.dataset = ds.DataSet(
+            self.dummy_df,
+            test_data=self.dummy_df,
+            string_features=['feature3']
         )
 
-        self.assertEqual(dataset.dataset_file, 'test.h5')
-        self.assertTrue(isinstance(dataset.train_data, pd.DataFrame))
-        self.assertTrue(isinstance(dataset.test_data, pd.DataFrame))
-        self.assertTrue(isinstance(dataset.string_features, list))
-        self.assertEqual(dataset.string_features[0], 'feature1')
 
+    def test_generate_data_pipeline(self):
+        '''Tests the data pipeline generation function.'''
 
-if __name__ == '__main__':
-    unittest.main()
+        pipeline=self.dataset._generate_data_pipeline()
+
+        self.assertGreater(len(pipeline), 1)
+
+        for operation, parameters in pipeline.items():
+            self.assertTrue(isinstance(operation, str))
+            self.assertTrue(isinstance(parameters, dict))
+
